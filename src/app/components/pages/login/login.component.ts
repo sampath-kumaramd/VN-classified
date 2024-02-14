7b80d605bf6f5fd3193ae4d82d84d2c2112479a3
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-02-14 13:41:11 +0530

    add validation to login and register form

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,29 +1,42 @@
 import { Component } from '@angular/core';
-import { Form, FormControl, FormGroup } from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
-  loginForm:FormGroup;
-  signupForm:FormGroup;
+  loginForm: FormGroup;
+  signupForm: FormGroup;
   hide = true;
-  constructor(){
+  constructor() {
     this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl('')
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
     });
 
     this.signupForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
-      firstName: new FormControl(''),
-      lastName: new FormControl('')
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
     });
-
   }
 
+  onLoginSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log('Login form is not valid');
+    }
+  }
+
+  onSignupSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    } else {
+      console.log('Signup form is not valid');
+    }
+  }
 }
